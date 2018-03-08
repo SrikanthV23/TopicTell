e5e1b6597bcf9392021e681146e91d5fc94388ca
SrikanthV23 <imsrikanth23@gamil.com>
2018-03-08 22:56:46 +0530

    git commit

--- js/src/script.js
+++ js/src/script.js
@@ -1,9 +1,9 @@
 var name=window.prompt("please enter your name");
 var username=document.getElementById("userName");
 username.innerHTML +=" " +name;
-
+var tD=document.getElementById("tripDura");
 function calcDuration(){
-	var tD=document.getElementById("tripDura");
+	
 	var tM=document.getElementById("tripMessage");
 	if(isNaN(tD.value)){
 		alert("please enter number");
@@ -26,4 +26,22 @@ function calcDuration(){
 	hDt.innerHTML += "That means you will be traveling for " + hours + " hours.";
 	var timeDt=document.getElementById("timeDuration");
 	timeDt.innerHTML += "That means you will be traveling for " + time + " minutes.";
+}
+
+var budgetCalc=document.getElementById("calcBudget");
+var indianRupee= 65.14;
+budgetCalc.onclick = function(){
+	var usAmount = document.getElementById("usAmount");
+	var convAmount=usAmount.value * indianRupee;
+	var perDay = convAmount/tD.value; 
+	var tenUsd =convAmount / 10; 
+	var fiveOOUsd =convAmount / 500; 
+	var totalCost=document.getElementById("totalCost");
+	var perDayCost=document.getElementById("totalCostpday");
+	var tenDollars=document.getElementById("tenDollars1");
+	var fiveOODollars=document.getElementById("fiveOODollars1");
+	totalCost.innerHTML = "Total cost for your journey is" + convAmount + "Rs" ;
+	perDayCost.innerHTML = "It looks you like to spend" +  perDay + "per day";
+	tenDollars.innerHTML = "For 10 USD it costs" +  tenUsd + "Rs";
+	fiveOODollars.innerHTML = "For 500 USD it costs" +  fiveOOUsd + "Rs" ;
 }
